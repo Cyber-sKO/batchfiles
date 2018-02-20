8433776de656c25181cbcc2a1ad7240311e5a19f
Cyber_sKO <cyber.sko@gmail.com>
2018-02-20 03:52:08 +0100

    make Scripts great again!

--- wlan-setter/wlan.bat
+++ wlan-setter/wlan.bat
@@ -1,36 +1,31 @@
 :: created by Cyber_sKO
-:: lc 29.03~0.1
-:: wt 35m
+:: Last Chance 20.2.18
 @echo off
 @color 0a
-@title ----- Wlan-Settings -----
+@title --- Hotspot-Konfigurator ---
 
-REM --- Setzten ---
-:int
+:set
 set keyusage=persistent
-goto wahl
-:wahl
+goto menu
+:menu
 cls
 echo.
 echo Bitte auswaehlen:
 echo.
-echo #==============================#
-echo #                              #
-echo #        1 = set Wlan          #
-echo #                              #
-echo #        2 = start Wlan        #
-echo #                              #
-echo #        3 = stop Wlan         #
-echo #                              #
-echo #          4 = exit            #
-echo #                              #
-echo #==============================#
+echo *=================================*
+echo #                                 #
+echo #     1 = Hotspot Einstellen      #
+echo #                                 #
+echo #   2 = Hotspot Starten/Stoppen   #
+echo #                                 #
+echo #          3 = Verlassen          #
+echo #                                 #
+echo *=================================*
 echo.
-choice /n /c 1234
-	if errorlevel 255 goto int1
-	if errorlevel 4 goto exit
-	if errorlevel 3 goto stop
-	if errorlevel 2 goto start
+choice /n /c 123
+	if errorlevel 255 goto set
+	if errorlevel 3 goto exit
+	if errorlevel 2 goto startstop
 	if errorlevel 1 goto set
 REM --- error ---
 goto e1
@@ -53,83 +48,72 @@ REM --- Möglichkeiten ---
 :set
 cls
 echo.
-echo Hier werden ihre Einstellungen fuer das Wlan festgelegt.
+echo Hier werden ihre Einstellungen fuer den Hotspot festgelegt.
 echo kann man immer nach belieben veraendern.
-echo bitte alles ausfuellen
-:: echo was schon festgelegt wurde was man nicht verändern bitte weiter drücken.
-pause>nul
+echo.
+echo ALLES AUSFUELLEN!
+ping /n 4 localhost>nul
 cls
 echo.
-echo Bitte geben sie ihr Wlan einen Namen:
+echo Wie soll der Hotspot heissen?
 set /p ssid=
 cls
 echo.
-echo Bitte geben sie ihr Wlan ein Passwort zum verbinden
+echo Welches Passwort soll der Hotspot haben?
 set /p key=
 cls
 echo.
-:: echo Soell das Wlan gestartet werden sobald der PC hochfaehrt?
-:: echo ---- funktion hat noch keine Funktion! ----
-:: echo j/n
-:: set /p as=
-:: cls
 :wlanset
 :: --- Wlan set´s mit Bedingungen
 netsh wlan set hostednetwork mode=allow
-goto 1
-:1
-if "%ssid%"=="" goto 2
 netsh wlan set hostednetwork ssid=%ssid%
-:2
-if "%key%"=="" goto check
 netsh wlan set hostednetwork key=%key% keyUsage=%keyusage%
-goto check
-
-:check
-:: -----
-:: wegen den herausnehmen der funktion
-goto int
-:: -----
-:: if %as%==j goto mkauto
-:: if %as%==y goto mkauto
-:: if %as%==ja goto mkauto
-
-:mkauto 
-if exist wlanstartcyber.bat goto exist
-mkdir C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\wlanstartcyber.bat
-echo :: created by Cyber_sKO>>"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\wlanstartcyber.bat"
-echo @ echo off>>"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\wlanstartcyber.bat"
-echo netsh wlan start hostednetwork>>"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\wlanstartcyber.bat"
-goto int 
+cls
+echo.
+echo Hotspot konfiguriert.
+ping /n 3 localhost>nul
+goto set
 :: ------------------------------------------------------------------------
-:start
+:startstop
 cls
 echo.
-echo Das Netztwerk wird nun gestarten.
-ping /n 2 localhost>nul
-netsh wlan start hostednetwork
-ping /n 1 localhost>nul
-cls
-echo.
-echo Wlan wurde gestartet.
-ping /n 3 localhost>nul
-goto int
-:stop
-cls
-echo.
-echo Das Netztwerk wird nun beendet.
-ping /n 2 localhost>nul
-netsh wlan stop hostednetwork
-ping /n 1 localhost>nul
-cls
-echo.
-echo Wlan wurde gestopt.
-ping /n 3 localhost>nul
-goto int
-
+echo *============================*
+echo #                            #
+echo #          1 = Start         #
+echo #                            #
+echo #          2 = Stop          #    
+echo #                            #
+echo *============================*
+choice /n /c 12
+	if errorlevel 255 goto startstop
+	if errorlevel 2 goto stop
+	if errorlevel 1 goto start
+goto e1
+		:start
+		cls
+		echo.
+		echo Hotspot wird gestartet.
+		ping /n 2 localhost>nul
+			netsh wlan start hostednetwork
+		cls
+		echo.
+		echo Hotspot wurde gestartet.
+		ping /n 3 localhost>nul
+		goto set
+		:stop
+		cls
+		echo.
+		echo Hotspot wird gestopt.
+		ping /n 2 localhost>nul
+			netsh wlan stop hostednetwork
+		cls
+		echo.
+		echo Hotspot wurde gestopt.
+		ping /n 3 localhost>nul
+		goto set	
 :exit
 cls
 echo.
 echo Danke fuer das Benutzen des CMD Skript von Cyber_sKO
-ping /n 2 localhost>nul
+ping /n 3 localhost>nul
 exit
